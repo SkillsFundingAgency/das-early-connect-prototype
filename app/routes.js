66925bb945561b3e6c14f5566f7e69626d4105ba
ls-nomensa <93996837+ls-nomensa@users.noreply.github.com>
2023-09-11 12:16:39 +0100

    Create version 2

--- app/routes.js
+++ app/routes.js
@@ -43,3 +43,41 @@ router.post('/feature/competitive-salary/v1/wages/action', function (req, res) {
         res.redirect('/feature/competitive-salary/v1/extra-info-pay')
     }
 })
+
+// V2
+
+router.post('/feature/multiple-locations/v2/action/', function (req, res) {
+    var apprenticeLocation = req.session.data['selected-location']
+
+    if (apprenticeLocation == "multiple-locations") {
+        res.redirect('/feature/multiple-locations/v2/multiple-locations')
+    }
+    else {
+        res.redirect('/feature/multiple-locations/v2/task-list')
+    }
+})
+router.post('/feature/no-provider/v2/action/', function (req, res) {
+    var providerKnow = req.session.data['provider-known']
+
+    if (providerKnow == "yes") {
+        res.redirect('/feature/no-provider/v2/select-training-provider')
+    }
+    else {
+        res.redirect('/feature/no-provider/v2/task-list')
+    }
+})
+
+
+router.post('/feature/competitive-salary/v2/wages/action', function (req, res) {
+    var WageType = req.session.data['WageType']
+
+    if (WageType == "competitiveWage") {
+        res.redirect('/feature/competitive-salary/v2/competitive-min-wage')
+    }
+    else if(WageType == "fixedWage") {
+        res.redirect('/feature/competitive-salary/v2/set-wage')
+    }
+    else {
+        res.redirect('/feature/competitive-salary/v2/extra-info-pay')
+    }
+})

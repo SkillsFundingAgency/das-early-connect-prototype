63428b5ac1fa2033cadb830eb4b1965c1c192f33
ls-nomensa <93996837+ls-nomensa@users.noreply.github.com>
2024-02-21 11:56:28 +0000

    Update to crown logo

--- node_modules/hmrc-frontend/hmrc/all-govuk-and-hmrc.js
+++
@@ -1,8 +0,0 @@
-import GOVUKFrontend from 'govuk-frontend/govuk/all';
-import HMRCFrontend from './all';
-
-window.GOVUKFrontend = GOVUKFrontend;
-window.HMRCFrontend = HMRCFrontend;
-
-GOVUKFrontend.initAll();
-HMRCFrontend.initAll();
